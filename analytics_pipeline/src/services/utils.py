c13ef76d55bef407e5101480e0c59fd4ae46bfc8
Shankara Narayana N G <shankarnarayana92@gmail.com>
2026-03-25 09:38:23 +0000

    Updated the files and folder structure

---
+++ analytics_pipeline/src/services/utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+
+baseDIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+def load_data(file_name):
+    try:
+        file_path = os.path.join(baseDIR, 'data', file_name)
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
