7dadae145fe102dd100e70a12f1be08d98225910
Shankara Narayana N G <shankarnarayana92@gmail.com>
2026-04-21 00:44:35 +0100

    Updated the flow

--- analytics_pipeline/src/services/utils.py
+++ analytics_pipeline/src/services/utils.py
@@ -1,3 +1,4 @@
+import joblib
 import pandas as pd
 import numpy as np
 import os
@@ -23,3 +24,36 @@ def save_data(data, file_name):
     except Exception as e:
         print(f"Error saving data: {e}")
         return None
+
+def save_model(model, subdirectory, file_name):
+    try:
+        dir_path = os.path.join(baseDIR, 'models', subdirectory)
+        os.makedirs(dir_path, exist_ok=True)
+        file_path = os.path.join(dir_path, file_name)
+        joblib.dump(model, file_path)
+    except Exception as e:
+        print(f"Error saving model: {e}")
+
+
+def load_model(subdirectory, file_name):
+    try:
+        file_path = os.path.join(baseDIR, 'models', subdirectory, file_name)
+        model = joblib.load(file_path)
+        return model
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        return None
+    
+def saveplot(plot, subdirectory, file_name):
+    try:
+        clean_name = file_name
+        if ':' in clean_name:
+            clean_name = clean_name.replace(':', '')
+        if ' ' in clean_name:
+            clean_name = "_".join(clean_name.split())
+        dir_path = os.path.join(baseDIR, 'models', 'artifacts', subdirectory)
+        os.makedirs(dir_path, exist_ok=True)
+        file_path = os.path.join(dir_path, f"{clean_name}.png")
+        plot.savefig(file_path)
+    except Exception as e:
+        print(f"Error saving plot: {e}")
